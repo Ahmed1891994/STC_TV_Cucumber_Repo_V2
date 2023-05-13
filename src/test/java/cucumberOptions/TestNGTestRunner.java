507b1065103f69560b596051c71fb466ca758a50
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-05-14 02:00:23 +0300

    first commit

---
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -0,0 +1,23 @@
+package cucumberOptions;
+
+import org.testng.annotations.DataProvider;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features={"src/test/resources/features"},glue ={"stepDefinitions"}
+,monochrome=true,
+plugin= {"pretty",
+		"html:target/cucumber.html",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		"json:target/cucumber-report.json",
+		"junit:target/cucumber-reports/Cucumber.xml",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+		)
+public class TestNGTestRunner extends AbstractTestNGCucumberTests{
+	@Override
+	@DataProvider(parallel = true)
+	public Object[][] scenarios()
+	{
+		return super.scenarios();
+	}
+}
