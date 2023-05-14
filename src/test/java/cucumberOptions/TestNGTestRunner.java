99efb84f88e6c987ced521582004e5ad3ef066d1
Ahmed1891994 <ahmedhakem189@yahoo.com>
2023-05-14 23:37:03 +0200

    Edit path of reports

--- src/test/java/cucumberOptions/TestNGTestRunner.java
+++ src/test/java/cucumberOptions/TestNGTestRunner.java
@@ -7,10 +7,11 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(features={"src/test/resources/features"},glue ={"stepDefinitions"}
 ,monochrome=true,
 plugin= {"pretty",
-		"html:target/cucumber.html",
+		"html:test-output/cucumberReports.html",
 		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-		"json:target/cucumber-report.json",
-		"junit:target/cucumber-reports/Cucumber.xml",
+		"json:test-output/cucumberReports.json",
+		"junit:test-output/cucumberReports/Cucumber.xml",
+		"rerun:test-output/failed_scenario.txt",
 		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
 public class TestNGTestRunner extends AbstractTestNGCucumberTests{
 	@Override
