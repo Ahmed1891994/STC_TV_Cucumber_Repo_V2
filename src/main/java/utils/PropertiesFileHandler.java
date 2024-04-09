2c7568b4c468ef5f92a5b9820f8793cc03939906
ahmed <ahmedhakem189@yahoo.com>
2024-04-09 03:04:35 +0300

    Improve Properties file handler

--- src/main/java/utils/PropertiesFileHandler.java
+++ src/main/java/utils/PropertiesFileHandler.java
@@ -4,16 +4,18 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+
 public class PropertiesFileHandler {
-    //load properties file from folder
-    public Properties loadProperties(String resourceName) throws IOException {
+    // Load properties file from folder
+    public Properties loadProperties(String resourceName) {
         Properties prop = new Properties();
-        try (FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "/src/main/resources/configurations/" + resourceName + ".properties")) {
+        String filePath = System.getProperty("user.dir") + "/src/main/resources/configurations/" + resourceName + ".properties";
+        try (FileInputStream fis = new FileInputStream(filePath)) {
             MyLogger.info("Loaded property file: " + resourceName);
             prop.load(fis);
         } catch (IOException e) {
-            MyLogger.error("Error loading property file: " + resourceName);
-            throw e;
+            MyLogger.error("Error loading property file: " + resourceName, e);
+            throw new RuntimeException("Error loading property file: " + resourceName, e);
         }
         return prop;
     }
